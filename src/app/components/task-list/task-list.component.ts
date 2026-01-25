9298079c68700002596737bfe30c854e8416d43b
Ayman el founti <aymaneelfounti@gmail.com>
2026-01-25 23:55:25 +0100

    commit taskList

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
-  templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  imports: [CommonModule],
+  templateUrl: './task-list.component.html'
 })
 export class TaskListComponent {
+  tasks = this.taskService.tasks;
 
+  constructor(private taskService: TaskService) {}
 }

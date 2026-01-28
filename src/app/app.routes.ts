e24813118850b18c2f4c9761888dff5ffe95e1f7
Ayman el founti <aymaneelfounti@gmail.com>
2026-01-28 13:50:24 +0100

    commit routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+    { path: 'tasks', component: TaskListComponent },
+    { path: 'tasks/:id', component: TaskDetailComponent }
+];

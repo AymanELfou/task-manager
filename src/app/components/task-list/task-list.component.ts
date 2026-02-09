0cbd399fe3d0992540c77cc8cd763b28b98ba945
Ayman el founti <aymaneelfounti@gmail.com>
2026-02-09 23:54:30 +0100

    add task-form Css

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
 import { TaskService } from '../../services/task.service';
-import { CommonModule } from '@angular/common';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
